d3b6e51fe4e119b0d6d13300ae5016056c8ef2ae
John <dazajohn08@gmail.com>
2026-03-15 19:23:18 -0700

    done1

--- main.cpp
+++ main.cpp
@@ -8,11 +8,13 @@ int main()
     double F;
 
     // TODO: [Step 1] Input a Celcius degree as a "double" type value
-
+cin >> C;
     // TODO: [Step 2] Convert it to a Fahrenheit degree using the Formula F = (9 / 5.0)* C + 32
-
+F = (9/5.0) * C + 32;
     cout << setprecision(2) << fixed;
     // TODO: [Step 3] Print the converted Fahrenheit
+    cout << setprecision <<(2) << fixed;
+    cout << F << endl;
 
     return 0;
 }
